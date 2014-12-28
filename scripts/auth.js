d4a7847d552ea65bb98eaff0b15d30f8ed96e74f
GarmashovK <garmashov.konstantin@yandex.ru>
2014-12-28 23:23:27 +0300

    Auth js added

---
+++ scripts/auth.js
@@ -0,0 +1,16 @@
+// JavaScript source code
+$(document).ready(function () {
+    VK.Auth.login(authInfo);
+});
+VK.init({
+    apiId: 4703399
+});
+
+function authInfo(response) {
+    if (response.session) {
+        alert('user: ' + response.session.mid);
+    } else {
+        alert('not auth');
+    }
+}
+VK.Auth.getLoginStatus(authInfo);
