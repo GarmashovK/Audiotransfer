755cb19b0caf854409e449df3f2a6f2784d30b77
GarmashovK <garmashov.konstantin@yandex.ru>
2015-01-03 23:39:22 +0300

    test with angular

--- scripts/app.js
+++ scripts/app.js
@@ -1,29 +1,70 @@
-var app = angular.module('vktodeezer', ['vktodeezer-controllers', 'vktodeezer-auth']);
-app.
+var app = angular.module('vktodeezer', []);
+//app.
 
-config(function ()
-{
-    VK.init({
-        apiId: 4703399
-    });
+//config(function ()
+//{
+//    //VK.init({
+//    //    apiId: 4703399
+//    //});
 
-    DZ.init({
-        appId: 149671,
-        channelUrl: 'http://vktodeezer.azurewebsites.net'
-    });
-}).
-
-run(['$scope', '$log', function ($scope, $log)
-{
-    VK.Auth.getLoginStatus(VKAuthInfo);
-    DZ.getLoginStatus(DZAuthInfo);
+//    //DZ.init({
+//    //    appId: 149671,
+//    //    channelUrl: 'http://vktodeezer.azurewebsites.net'
+//    //});
+//}).
+//run(['$scope', '$log', function ($scope, $log)
+//{
+//    //VK.Auth.getLoginStatus(VKAuthInfo);
+//    //DZ.getLoginStatus(DZAuthInfo);
     
-    $scope.DZAuthInfo = function (response)
+//    //$scope.DZAuthInfo = function (response)
+//    //{
+//    //    if (response.authResponse)
+//    //    {
+//    //        $scope.dzuid = response.userID | response.authResponse.userID;
+//    //        $log.log('Success login DZ!');
+//    //    } else
+//    //    {
+//    //        $log.log('Login is failed DZ!');
+//    //    }
+//    //}
+
+//    //$scope.VKAuthInfo = function (response)
+//    //{
+//    //    if (response.session)
+//    //    {
+//    //        $scope.vkuid = response.session.mid;
+//    //        $log.log('Success login VK!');
+//    //    } else
+//    //    {
+//    //        $log.log('Login is failed VK!');
+//    //    }
+//    //}
+
+//    $scope.VKAuthClick = function ()
+//    {
+//        $log.log("VKAuthClick");
+//        //VK.Auth.login($scope.VKAuthInfo, 8);
+//    }
+
+//    $scope.DZAuthClick = function ()
+//    {
+//        $log.log("DZAuthClick");
+//        //DZ.login($scope.DZAuthInfo, {
+//        //    perms: 'basic_access,manage_library,delete_library'
+//        //});
+//    }
+//}]);
+
+app.controller('AuthController', ['$log', function ($log)
+{
+    var scope = this;
+
+    this.DZAuthInfo = function (response)
     {
         if (response.authResponse)
         {
-            $scope.dzuid = response.userID | response.authResponse.userID;
-            //$('#DZAuthBtn').fadeOut();
+            scope.dzuid = response.userID | response.authResponse.userID;
             $log.log('Success login DZ!');
         } else
         {
@@ -31,15 +72,27 @@ run(['$scope', '$log', function ($scope, $log)
         }
     }
 
-    $scope.VKAuthInfo = function (response)
+    this.VKAuthInfo = function (response)
     {
         if (response.session)
         {
-            $scope.vkuid = response.session.mid;
+            scope.vkuid = response.session.mid;
             $log.log('Success login VK!');
         } else
         {
             $log.log('Login is failed VK!');
         }
+    }
+
+    this.VKAuthClick = function ()
+    {
+        VK.Auth.getLoginStatus(scope.VKAuthClick, 8);
+    }
+    
+    this.DZAuthClick = function ()
+    {
+        DZ.login(scope.DZAuthInfo, {
+            perms: 'basic_access,manage_library,delete_library'
+        });
     }
 }]);
