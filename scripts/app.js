01017d95edc5b3ea5eb4c7cb212b07a3907414fb
GarmashovK <garmashov.konstantin@yandex.ru>
2015-01-07 02:30:41 +0300

    this to

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,24 @@
 var app = angular.module('vktodeezer', []);
 
-app.controller('AuthController', ['$log', '$window', function ($log, $window)
+app.controller('AuthController', ['$scope', '$log', '$window', function ($scope, $log, $window)
 {
-    var scope = this;
-    this.tracks = [];
-    this.tracks_offset = 0;
-    this.tracks_count = 10;
+    $scope.tracks = [];
+    $scope.tracks_offset = 0;
+    $scope.tracks_count = 10;
 
-    this.clearTracks = function () { tracks = [];}
+    $scope.clearTracks = function () { tracks = []; }
 
-    this.addTrack = function (track)
+    $scope.addTrack = function (track)
     {
-        scope.tracks.push(track);
-        scope.tracks_offset++;
+        $scope.tracks.push(track);
+        $scope.tracks_offset++;
     }
 
-    this.DZAuthInfo = function (response)
+    $scope.DZAuthInfo = function (response)
     {
         if (response.authResponse)
         {
-            scope.dzuid = response.userID | response.authResponse.userID;
+            $scope.dzuid = response.userID | response.authResponse.userID;
             $log.log('Success login DZ!');
         } else
         {
@@ -27,25 +26,25 @@ app.controller('AuthController', ['$log', '$window', function ($log, $window)
         }
     }
 
-    this.VKAuthInfo = function (response)
+    $scope.VKAuthInfo = function (response)
     {
         if (response.session)
         {
-            scope.vkuid = response.session.mid;
+            $scope.vkuid = response.session.mid;
             $log.log('Success login VK!');
         } else
         {
             $log.log('Login is failed VK!');
         }
     };
-    
-    this.loadTracks = function ()
+
+    $scope.loadTracks = function ()
     {
         VK.Api.call('audio.get', {
-            owner_id: scope.vkuid,
+            owner_id: $scope.vkuid,
             need_user: 0,
-            offset: scope.tracks_offset,
-            count: scope.tracks_count
+            offset: $scope.tracks_offset,
+            count: $scope.tracks_count
         },
         function (data)
         {
@@ -54,29 +53,29 @@ app.controller('AuthController', ['$log', '$window', function ($log, $window)
             {
                 for (var i = 1; i < data.response.length; i++)
                 {
-                    scope.addTrack(data.response[i]);
+                    $scope.addTrack(data.response[i]);
                 }
             } else
             {
                 $log.log('bad request');
             }
-            $log.log(scope.tracks);
+            $log.log($scope.tracks);
         });
     };
 
-    this.VKAuthClick = function ()
+    $scope.VKAuthClick = function ()
     {
-        VK.Auth.login(scope.VKAuthClick, 8);
+        VK.Auth.login($scope.VKAuthClick, 8);
     };
 
-    this.DZAuthClick = function ()
+    $scope.DZAuthClick = function ()
     {
-        DZ.login(scope.DZAuthInfo, {
+        DZ.login($scope.DZAuthInfo, {
             perms: 'basic_access,manage_library,delete_library'
         });
     };
 
-    this.VKLogoutClick = function ()
+    $scope.VKLogoutClick = function ()
     {
         VK.Auth.logout(function (data)
         {
@@ -88,7 +87,7 @@ app.controller('AuthController', ['$log', '$window', function ($log, $window)
     $window.onload = function ()
     {
         $log.log('windowonload');
-        VK.Auth.getLoginStatus(scope.VKAuthInfo);
-        DZ.getLoginStatus(scope.DZAuthInfo);
+        VK.Auth.getLoginStatus($scope.VKAuthInfo);
+        DZ.getLoginStatus($scope.DZAuthInfo);
     }
 }]);
