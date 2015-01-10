5837772568601ffe5e203db34470ff65393f8f09
GarmashovK <garmashov.konstantin@yandex.ru>
2015-01-10 22:15:08 +0300

    big changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,94 +1,145 @@
 var app = angular.module('vktodeezer', []);
 
-app.controller('AuthController', ['$scope', '$log', '$window', function ($scope, $log, $window)
-{
-    $scope.tracks = [];
-    $scope.tracks_offset = 0;
-    $scope.tracks_count = 10;
-
-    $scope.clearTracks = function () { tracks = []; }
-
-    $scope.addTrack = function (track)
+app.service('VKService',
+    ['$rootScope', '$log',
+    function ($log, $rootScope)
     {
-        $scope.tracks.push(track);
-        $scope.tracks_offset++;
-    }
+        // ref to this
+        var service = this;
 
-    $scope.DZAuthInfo = function (response)
-    {
-        if (response.authResponse)
+        // auth handler
+        var VKAuthInfo = function (response)
         {
-            $scope.dzuid = response.userID | response.authResponse.userID;
-            $log.log('Success login DZ!');
-        } else
-        {
-            $log.log('Login is failed DZ!');
-        }
-    }
-
-    $scope.VKAuthInfo = function (response)
-    {
-        if (response.session)
-        {
-            $scope.vkuid = response.session.mid;
-            $log.log('Success login VK!');
-        } else
-        {
-            $log.log('Login is failed VK!');
-        }
-    };
-
-    $scope.loadTracks = function ()
-    {
-        VK.Api.call('audio.get', {
-            owner_id: $scope.vkuid,
-            need_user: 0,
-            offset: $scope.tracks_offset,
-            count: $scope.tracks_count
-        },
-        function (data)
-        {
-            $log.log(data);
-            if (data.response)
+            if (response.session)
             {
-                for (var i = 1; i < data.response.length; i++)
-                {
-                    $scope.addTrack(data.response[i]);
-                }
-                $scope.$digest();
+                // set user ID
+                service.vkuid = response.session.mid;
+
+                // sending success login event
+                $rootScope.$broadcast('vk_success_login');
+                //$log.log('Success login VK!');
             } else
             {
-                $log.log('bad request');
+                //sending failed login event
+                $rootScope.$broadcast('vk_failed_login');
+                //$log.log('Login is failed VK!');
             }
-            $log.log($scope.tracks);
-        });
-    };
+        }
 
-    $scope.VKAuthClick = function ()
-    {
-        VK.Auth.login($scope.VKAuthClick, 8);
-    };
-
-    $scope.DZAuthClick = function ()
-    {
-        DZ.login($scope.DZAuthInfo, {
-            perms: 'basic_access,manage_library,delete_library'
-        });
-    };
-
-    $scope.VKLogoutClick = function ()
-    {
-        VK.Auth.logout(function (data)
+        // tracks loading handling
+        var loadTracksHandler = function (data)
         {
-            $log.log('success logout!');
-            $log.log(data);
-        })
-    }
+            if (data.response)
+            {
+                var tracks = data.response;
+                tracks.shift();  //removing response data and get only tracks
 
-    $window.onload = function ()
+                // sending tracks to scope
+                $rootScope.$broadcast('vk_tracks_loaded', tracks);
+            } else
+            {
+                // something failed
+                $rootScope.$broadcast('vk_tracks_not_loaded');
+            }
+        };
+
+        /* login fun
+            permissions for app
+        */
+        this.login = function (permissions)
+        {
+            VK.Auth.login(VKAuthInfo, permissions);
+        }
+
+        // logout
+        this.logout = function ()
+        {
+            VK.Auth.logout(function (data)
+            {
+                $rootScope.$broadcast('vk_success_logout');
+                //$log.log('success logout!');
+                //$log.log(data);
+            });
+        }
+
+        //load from common tracklist
+        this.loadUserTracks = function (offset, count)
+        {
+            VK.Api.call('audio.get',
+                {
+                    // uid
+                    owner_id: service.vkuid,
+                    // user data (not necessary)
+                    need_user: 0,
+                    // offset for list in tracks
+                    offset: offset,
+                    // num of tracks for loading
+                    count: count
+                }, loadTracksHandler);
+        };
+
+        this.getLoginStatus = function ()
+        {
+            VK.Auth.getLoginStatus(VKAuthInfo);
+        }
+    }]);
+
+app.controller('AuthController',
+    ['$scope', '$log', '$window', 'VKService',
+    function ($scope, $log, $window)
     {
-        $log.log('windowonload');
-        VK.Auth.getLoginStatus($scope.VKAuthInfo);
-        DZ.getLoginStatus($scope.DZAuthInfo);
-    }
-}]);
+        $scope.tracks = [];
+
+        $scope.DZAuthInfo = function (response)
+        {
+            if (response.authResponse)
+            {
+                $scope.dzuid = response.userID | response.authResponse.userID;
+                $log.log('Success login DZ!');
+            } else
+            {
+                $log.log('Login is failed DZ!');
+            }
+        }
+
+        $scope.VKAuthClick = function ()
+        {
+            VK.Auth.login($scope.VKAuthInfo, 8);
+        };
+
+        $scope.DZAuthClick = function ()
+        {
+            DZ.login($scope.DZAuthInfo, {
+                perms: 'basic_access,manage_library,delete_library'
+            });
+        };
+
+        var count = 10;
+        var offset = 0;
+
+        $scope.loadTracks = function ()
+        {
+            VKService.loadUserTracks(offset, count);
+            $scope.on('vk_tracks_loaded', function (tracks)
+            {
+                $scope.tracks = tracks;
+                $scope.$digest();
+            })
+        }
+
+        $scope.VKLogoutClick = function ()
+        {
+            VK.Auth.logout(function (data)
+            {
+                $log.log('success logout!');
+                $log.log(data);
+            })
+        }
+
+        $window.onload = function ()
+        {
+            $log.log('windowonload');
+            $scope.on('vk_success_login', function () { $log.log('success login'); });
+            DZ.getLoginStatus($scope.DZAuthInfo);
+        }
+    }]);
